0fa2a165d0098b0f78c680648cc8276e8388e467
Hwanggyuun <gka365@naver.com>
2024-08-21 18:23:45 +0900

    first commit

--- 3/3-1.py
+++ 3/3-1.py
@@ -1,24 +1,19 @@
+
 N = int(input())
-dimensions = []
+hang = [] 
 
-# 행렬 차원 정보를 dimensions 배열에 저장
-for _ in range(N):
-    x, y = map(int, input().split())
-    if len(dimensions) == 0:
-        dimensions.append(x)  # 첫 번째 행렬의 행
-    dimensions.append(y)  # 행렬의 열
-
-# DP 테이블을 무한대로 초기화
 dp = [[float('inf')] * N for _ in range(N)]
-
-# 대각선의 길이
-for length in range(1, N):
-    for i in range(N - length):
-        j = i + length
-        if i == j:
+for i in range(N):
+    x,y = map(int, input().split())
+    if i == 0:
+        hang.append(x) 
+    hang.append(y)
+for len in range(1,N):
+    for i in range(N-len):
+        j= i +len
+        if i ==j:
             dp[i][j] = 0
         else:
-            for k in range(i, j):
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + dimensions[i] * dimensions[k + 1] * dimensions[j + 1])
-
-print(dp[0][N - 1])
+            for k in range(i,j):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + hang[i] * hang[k + 1] * hang[j + 1])
+print(dp[0][N-1])
